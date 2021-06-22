27a40d9692b99f5637899468f4f6209c420bb201
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-06-22 09:15:50 +0200

    commit label role

--- mymenu-backEnd/src/main/java/sopra/myMenubackEnd/model/Role.java
+++ mymenu-backEnd/src/main/java/sopra/myMenubackEnd/model/Role.java
@@ -1,5 +1,16 @@
 package sopra.myMenubackEnd.model;
 
 public enum Role {
-	ADMIN, USER
+	ADMIN("ADMIN"), USER("USER");
+	
+	private final String label;
+
+	private Role(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
 }
