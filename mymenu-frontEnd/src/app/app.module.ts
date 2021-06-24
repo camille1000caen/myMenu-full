41ae48ea652524ce983706dc4bcdf20f66224843
Solenn <delta@g.fr>
2021-06-24 16:32:00 +0200

    front ingred recette plat

--- mymenu-frontEnd/src/app/app.module.ts
+++ mymenu-frontEnd/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatComponent } from './plat/plat.component';
+import { RecetteComponent } from './recette/recette.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatComponent,
+    RecetteComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mymenu-frontEnd/src/app/plat/plat.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+import {PlatHttpService} from "./plat-http.service";
+import {Plat} from "../model/plat";
+
+
+
+@Component({
+  selector: 'app-plat',
+  templateUrl: './plat.component.html',
+  styleUrls: ['./plat.component.scss']
+})
+export class PlatComponent implements OnInit {
+
+  platForm: Plat = null;
+
+
+  constructor(private platService: PlatHttpService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+  list(): Array<Plat> {
+    return this.platService.findAll();
+  }
+
+
+
+  add() {
+    this.platForm = new Plat();
+
+  }
+
+  edit(id: number) {
+
+    this.platService.findById(id).subscribe(resp=> {
+      this.platForm = resp;
+
+    }, err => console.log(err));
+  }
+
+  save() {
+
+    if (!this.platForm.id) {
+      this.platService.create(this.platForm);
+    } else {
+      this.platService.modify(this.platForm).subscribe(resp => {
+        this.platService.load();
+      }, error => console.log(error));
+    }
+    this.platForm = null;
+  }
+
+  cancel() {
+    this.platForm = null;
+  }
+
+  delete(id: number) {
+    this.platService.deleteById(id);
+  }
+
+}
+
+
+
