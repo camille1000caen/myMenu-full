5d062086610c978576d60cd2688581b17e2e7712
Favrot <tom@>
2021-06-25 09:51:00 +0200

    Component Header, Footer, Homebody

--- mymenu-frontEnd/src/app/app.module.ts
+++ mymenu-frontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomebodyComponent } from './homebody/homebody.component';
+import { ObjectifComponent } from './objectif/objectif.component';
+import { MenuComponent } from './menu/menu.component';
+import { PlanningComponent } from './planning/planning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomebodyComponent,
+    ObjectifComponent,
+    MenuComponent,
+    PlanningComponent
   ],
   imports: [
     BrowserModule,
