2e6eb363f6443d0855e43e3de1051e4a9be638a0
Solenn <delta@g.fr>
2021-06-25 10:40:06 +0200

    Auto stash before merge of "Solenn" and "origin/main"

--- mymenu-frontEnd/src/app/app-routing.module.ts
+++ mymenu-frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RecetteComponent} from "./recette/recette.component";
+import {PlatComponent} from "./plat/plat.component";
 
 const routes: Routes = [
+<<<<<<< Updated upstream
+=======
+  {path:"recette", component: RecetteComponent},
+  {path:"plat", component: PlatComponent}
+>>>>>>> Stashed changes
 
 ];
 
