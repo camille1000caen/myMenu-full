96cb39457d3ef204048120f6b5026eac30270e0f
camille <c>
2021-06-26 19:56:27 +0200

    pdf un peu mieux NPM INSTALL OBLIGATOIRE

--- mymenu-frontEnd/src/app/app.module.ts
+++ mymenu-frontEnd/src/app/app.module.ts
@@ -24,6 +24,7 @@ import {ListeCoursesComponent} from "./liste-courses/liste-courses.component";
 import {AjustementQuantiteComponent} from "./ajustement-quantite/ajustement-quantite.component";
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- mymenu-frontEnd/src/app/ingredient/ingredient.component.ts
+++ mymenu-frontEnd/src/app/ingredient/ingredient.component.ts
@@ -4,6 +4,7 @@ import {Ingredient} from "../model/ingredient";
 import {IngredientHttpService} from "./ingredient-http.service";
 
 import { jsPDF } from "jspdf";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'app-ingredient',
@@ -32,17 +33,82 @@ ListByRecette(id: number) {
 
   pdfDownload() {
     var doc = new jsPDF();
-    var interligne = 10;
-    doc.setTextColor(255, 182, 18);
-    doc.text("Ingrédients : ",10, interligne);
-    doc.text("Quantité : ",70, interligne);
+    var interligne = 40;
+    var indentation=15;
+    var nbLignes=0;
+    var nbIngredient=0;
+    var nbPage=1;
+    var dateDuJour=new Date();
+    var mois=dateDuJour.getUTCMonth()+1;//décompte des mois de 0 à 11
+
+    doc.setDrawColor(23,162,184);
+    doc.setTextColor(23,162,184);
+    doc.text("Ma liste de courses du : "+dateDuJour.getUTCDate()+'/'+mois+'/'+dateDuJour.getUTCFullYear(),70, interligne-25);
+    doc.text("Ingrédients : ",indentation, interligne-8);
+    doc.text("Quantité (g) : ",indentation+50, interligne-8);
     doc.setTextColor(0, 0, 0);
+    if(this.ingredientService.findAll().length>1){
+      doc.setTextColor(23,162,184);
+        doc.text("Ingrédients : ",115, interligne-8);
+        doc.text("Quantité (g) : ",115+50, interligne-8);
+        doc.setTextColor(0, 0, 0);
+    }
 
     for (let ingred of this.ingredientService.findAll()){
-      interligne=interligne+7;
-      doc.text(ingred.nom,10, interligne);
-      doc.text(ingred.quantite.toString(), 70, interligne);
-  }
+      nbIngredient+=1;
+      if(nbIngredient%2!=0){
+        indentation=15;
+        doc.circle(indentation-6, interligne-2,2,"S");
+        doc.text(ingred.nom,indentation, interligne);
+        doc.text(ingred.quantite.toString(), indentation+60, interligne);
+        doc.line(indentation,interligne+1,indentation+85,interligne+1);
+        nbLignes=nbLignes+1;
+      }else{
+         indentation=115;
+        doc.circle(indentation-6, interligne-2,2,"S");
+        doc.text(ingred.nom,indentation, interligne);
+        doc.text(ingred.quantite.toString(), indentation+60, interligne);
+        doc.line(indentation,interligne+1,indentation+85,interligne+1);
+         interligne=interligne+7;
+      }
+      if((nbIngredient%70)==0){
+          nbPage+=1;
+          doc.text("2021 Copyright - MyMenu",70,290);
+          doc.addPage();
+          nbLignes=0;
+          interligne=40;
+          indentation=15;
+          doc.setTextColor(23,162,184);
+        doc.text("Ingrédients : ",indentation, interligne-8);
+        doc.text("Quantité (g) : ",indentation+50, interligne-8);
+          doc.setTextColor(0, 0, 0);
+        if(this.ingredientService.findAll().length>70*(nbPage-1)+1){
+          doc.setTextColor(23,162,184);
+          doc.text("Ingrédients : ",115, interligne-8);
+          doc.text("Quantité (g) : ",115+50, interligne-8);
+          doc.setTextColor(0, 0, 0);
+        }
+      }
+    }
+    doc.setTextColor(23,162,184);
+    doc.text("Ma liste personnelle : ",80, interligne+37);
+    doc.line(10,interligne+30,200,interligne+30);
+    doc.line(10,interligne+135,200,interligne+135);
+    doc.line(10,interligne+30,10,interligne+135);
+    doc.line(200,interligne+30,200,interligne+135);
+    doc.setTextColor(0, 0, 0);
+
+    var newInterligne=interligne+44;
+    for(var i=interligne+44; i<interligne+57;i++){
+      doc.circle(15, newInterligne-2,2,"S");
+      doc.line(20,newInterligne+1,72,newInterligne+1);
+      doc.circle(78, newInterligne-2,2,"S");
+      doc.line(81,newInterligne+1,133,newInterligne+1);
+      doc.circle(139, newInterligne-2,2,"S");
+      doc.line(142,newInterligne+1,194,newInterligne+1);
+      newInterligne+=7;
+    }
+    doc.text("2021 Copyright - MyMenu",70,290);
     doc.save("Ma liste de course.pdf");
   }
 
