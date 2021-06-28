894866376d171036b5d07193b301d92563c910ce
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-06-28 20:30:06 +0200

    commit

--- mymenu-backEnd/src/test/java/sopra/myMenubackEnd/MyMenuUtilisateurDataTest.java
+++ mymenu-backEnd/src/test/java/sopra/myMenubackEnd/MyMenuUtilisateurDataTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import sopra.myMenubackEnd.model.Adresse;
+import sopra.myMenubackEnd.model.Coordonnees;
 import sopra.myMenubackEnd.model.Genre;
 import sopra.myMenubackEnd.model.PhysicalActivity;
+import sopra.myMenubackEnd.model.PreferenceAlimentaire;
+import sopra.myMenubackEnd.model.TypeAlimentation;
 import sopra.myMenubackEnd.model.Utilisateur;
 import sopra.myMenubackEnd.repository.IAdresseRepository;
 import sopra.myMenubackEnd.repository.IPreferenceAlimentaireRepository;
@@ -74,18 +78,53 @@ public class MyMenuUtilisateurDataTest {
 		// CREATION UTILISATEUR
 				Utilisateur Camille = new Utilisateur();
 				try {
-					Solenn.setDateNaissance(sdf.parse("1995-08-14"));
+					Camille.setDateNaissance(sdf.parse("1993-01-01"));
 				} catch (ParseException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				Solenn.setGenre(Genre.FEMME);
-				Solenn.setImc(14.0F);
-				Solenn.setNom("Stefanski");
-				Solenn.setPrenom("Solenn");
-				Solenn.setPhysicalActivity(PhysicalActivity.INTENSIF);
-				Solenn.setPoids(54.5F);
-				Solenn.setTaille(155.0F);
+				Camille.setGenre(Genre.FEMME);
+				Camille.setImc(8.0F);
+				Camille.setNom("1000CAEN");
+				Camille.setPrenom("KMILLE");
+				Camille.setPhysicalActivity(PhysicalActivity.OCCASIONNEL);
+				Camille.setPoids(51.5F);
+				Camille.setTaille(190.0F);
+				
+		// CREATION PREF ALIM
+				PreferenceAlimentaire prefAlimbat = new PreferenceAlimentaire(TypeAlimentation.NONE, null);
+
+				prefAlimbat.setUtilisateur(baptiste);
+				
+		// CREATION PREF ALIM
+				PreferenceAlimentaire prefAlimtom = new PreferenceAlimentaire(TypeAlimentation.FLEXITARIEN, null);
+
+				prefAlimtom.setUtilisateur(tom);
+				
+		// CREATION PREF ALIM
+				PreferenceAlimentaire prefAlimsol = new PreferenceAlimentaire(TypeAlimentation.VEGAN, null);
+
+				prefAlimsol.setUtilisateur(Solenn);
+				
+		// CREATION PREF ALIM
+				PreferenceAlimentaire prefAlimcam = new PreferenceAlimentaire(TypeAlimentation.HALAL, null);
+
+				prefAlimcam.setUtilisateur(Camille);
+
+		// CREATION ADRESSE
+				Adresse adr = new Adresse("33 rue IKEA", "Residence plikepluck", "33000", "Bordeaux", "France");
+				// adr.setUtilisateur(tom);
+				adr.setCoordonnees(new Coordonnees(4.0, 88.0));
+
+				// tom.addAdresse(adr);
+				
+		// CREATION ADRESSE
+				Adresse adr = new Adresse("33 rue IKEA", "Residence plikepluck", "33000", "Bordeaux", "France");
+				// adr.setUtilisateur(tom);
+				adr.setCoordonnees(new Coordonnees(4.0, 88.0));
+
+				// tom.addAdresse(adr);
+
 
 	}
 }	
